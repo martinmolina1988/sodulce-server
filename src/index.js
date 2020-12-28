cddee09b9ebd47420e6dec839e8edaf6feeb66d7
martinmolina1988 <martincarp35@gmial.com>
2020-12-28 19:49:04 -0300

    cuarto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
-if (process.env.NODE_ENV !== 'production') {
 
-    require("dotenv").config();
-}
+
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const multer = require("multer");
