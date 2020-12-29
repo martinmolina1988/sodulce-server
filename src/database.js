5478e205b5139354be1c6388f18f6c51be21abdf
martinmolina1988 <martincarp35@gmial.com>
2020-12-28 22:20:28 -0300

    makes

--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect(process.env.MongoAtlasUri, {
+mongoose.connect("mongodb+srv://martin:dodoria@cluster0-gdxja.mongodb.net/sodulce?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => console.log("Conectado a la base de datos")).catch(err => console.error(err));
-
